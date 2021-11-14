616aee3006ecca96850a9c68a879b751e91c3047
grand ahmad <grand23@grands-MacBook-Pro.local>
2021-11-14 14:01:01 +0300

    Atyaf

---
+++ TableView20/TableView20/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  TableView20
+//
+//  Created by grand ahmad on 08/04/1443 AH.
+//
+
+import Foundation
+struct Model {
+    var title: String
+    var description: String
+    var imageName: String
+}
