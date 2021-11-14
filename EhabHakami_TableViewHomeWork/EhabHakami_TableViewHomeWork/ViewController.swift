85e626398fd100618a6d35a8dd55def541427b43
Ehab hakami <ehab7k@gmail.com>
2021-11-14 16:53:11 +0300

    ehabUpdate2

--- EhabHakami_TableViewHomeWork/EhabHakami_TableViewHomeWork/ViewController.swift
+++ EhabHakami_TableViewHomeWork/EhabHakami_TableViewHomeWork/ViewController.swift
@@ -26,19 +26,25 @@ class Car{
 }
 
 class ViewController: UIViewController {
-    var titleArray = ""
+    var nameCC = ""
     
-    var dodge = Car(imageCar: UIImage(named: "charger_Srt")!, nemeCar: "charger", colorCar: "red", idNumberCar: "2020ssss121")
+    var imageCC = UIImage(named: "")
     
-    var viper = Car(imageCar: UIImage(named: "viper")!, nemeCar: "viper", colorCar: "white", idNumberCar: "2018vvva1223")
+    var colorCC = ""
     
-    var corvete = Car(imageCar: UIImage(named: "c8")!, nemeCar: "corveteC8", colorCar: "orange", idNumberCar: "2020cc1219")
+    var dodge = Car(imageCar: UIImage(named: "charger_Srt")!, nemeCar: "charger", colorCar: "red", idNumberCar: "musleCar")
     
-    var challenger = Car(imageCar: UIImage(named: "Challenger_Srt")!, nemeCar: "Challenger_Srt", colorCar: "red", idNumberCar: "2020fhdhf")
+    var viper = Car(imageCar: UIImage(named: "viper")!, nemeCar: "viper", colorCar: "white", idNumberCar: "superCar")
     
-    var comaro = Car(imageCar: UIImage(named: "comarozl1")!, nemeCar: "comarozl1", colorCar: "black", idNumberCar: "20201ofkfl")
+    var corvete = Car(imageCar: UIImage(named: "c8")!, nemeCar: "corveteC8", colorCar: "orange", idNumberCar: "superCar")
     
-var myarray = ["charger_Srt","Challenger_Srt","comarozl1","viper","c8"]
+    var challenger = Car(imageCar: UIImage(named: "Challenger_Srt")!, nemeCar: "Challenger_Srt", colorCar: "red", idNumberCar: "musleCar")
+    
+    var comaro = Car(imageCar: UIImage(named: "comarozl1")!, nemeCar: "comarozl1", colorCar: "black", idNumberCar: "musleCar")
+    
+    var myarray:[Car] = []
+    var myarrayTwo:[Car] = []
+    var myarrayTree:[[Car]] = []
     
     @IBOutlet weak var myTableViewinVc: UITableView!
     
@@ -52,11 +58,24 @@ var myarray = ["charger_Srt","Challenger_Srt","comarozl1","viper","c8"]
         myTableViewinVc.delegate = self
         myTableViewinVc.dataSource = self
         
-    
+        
+        // في الابند نقلنا البيانات الي في الكلاس حطيناها في الاراي بعد م عرفناها في متغير
+        myarray.append(dodge)
+        myarray.append(challenger)
+        myarray.append(comaro)
+        myarray.append(corvete)
+        myarray.append(viper)
+
+        
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let rootVC = segue.destination as! ViewControllerTwo
-        rootVC.recivedData = titleArray
+        
+        //نقل البيانات للفيو تو
+        rootVC.carName = nameCC
+        rootVC.imageCar = imageCC
+        rootVC.discrabionCar = colorCC
         
     }
 
@@ -71,52 +90,78 @@ extension ViewController:UITableViewDelegate,UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewConect
-        //var content = cell.defaultContentConfiguration()
-        let myarray = myarray[indexPath.row]
         
-        cell.labelNameCar.text = myarray
+        // add cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        
+        
+        var content = cell.defaultContentConfiguration()
+        
+        cell.accessoryType = .disclosureIndicator // button
+        
+        
+        content.text = myarray[indexPath.row].nemeCar
+        
+        content.secondaryText = myarray[indexPath.row].colorCar
+        
+        /////
+        
+        let sizeImage = CGSize(width: 70, height: 70)// size image
+        
+        content.image = myarray[indexPath.row].imageCar.preparingThumbnail(of: sizeImage)
+        content.imageProperties.maximumSize = sizeImage
+        content.image = myarray[indexPath.row].imageCar
+        
+        // end add and edit image
+        
+        
+        cell.contentConfiguration = content // print data in TableView
+        
+        //cell.labelNameCar.text = myarray
         
 //        cell.imageViewInCell.image = UIImage(named: myarray)
         
         
-        if cell.labelNameCar.text == "charger_Srt" {
+//        if cell.labelNameCar.text == "charger_Srt" {
+//
+//            cell.labelColerName.text = dodge.colorCar
+//            cell.imageViewInCell.image = dodge.imageCar
+//            cell.labelIdnumber.text = dodge.idNumberCar
+//
+//        }else if cell.labelNameCar.text == "Challenger_Srt"{
+//
+//            cell.labelColerName.text = challenger.colorCar
+//            cell.imageViewInCell.image = challenger.imageCar
+//            cell.labelIdnumber.text = challenger.idNumberCar
+//
+//        }else if cell.labelNameCar.text == "comarozl1"{
+//
+//            cell.labelColerName.text = comaro.colorCar
+//            cell.imageViewInCell.image = comaro.imageCar
+//            cell.labelIdnumber.text = comaro.idNumberCar
+//
+//        }else if cell.labelNameCar.text == "viper"{
+//            cell.labelColerName.text = viper.colorCar
+//            cell.imageViewInCell.image = viper.imageCar
+//            cell.labelIdnumber.text = viper.idNumberCar
+//        }else{
+//            cell.labelColerName.text = corvete.colorCar
+//            cell.imageViewInCell.image = corvete.imageCar
+//            cell.labelIdnumber.text = corvete.idNumberCar
             
-            cell.labelColerName.text = dodge.colorCar
-            cell.imageViewInCell.image = dodge.imageCar
-            cell.labelIdnumber.text = dodge.idNumberCar
             
-        }else if cell.labelNameCar.text == "Challenger_Srt"{
-            
-            cell.labelColerName.text = challenger.colorCar
-            cell.imageViewInCell.image = challenger.imageCar
-            cell.labelIdnumber.text = challenger.idNumberCar
-            
-        }else if cell.labelNameCar.text == "comarozl1"{
-            
-            cell.labelColerName.text = comaro.colorCar
-            cell.imageViewInCell.image = comaro.imageCar
-            cell.labelIdnumber.text = comaro.idNumberCar
-            
-        }else if cell.labelNameCar.text == "viper"{
-            cell.labelColerName.text = viper.colorCar
-            cell.imageViewInCell.image = viper.imageCar
-            cell.labelIdnumber.text = viper.idNumberCar
-        }else{
-            cell.labelColerName.text = corvete.colorCar
-            cell.imageViewInCell.image = corvete.imageCar
-            cell.labelIdnumber.text = corvete.idNumberCar
-            
-            
-        }
+        //}
         
         return cell
         
-    }
+    }// end func TO
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        titleArray = myarray[indexPath.row]
+        nameCC = myarray[indexPath.row].nemeCar
+        imageCC = myarray[indexPath.row].imageCar
+        colorCC = myarray[indexPath.row].idNumberCar
         performSegue(withIdentifier: "goToViewControler", sender: self)
     }
-    
+    // حق الطباعة في الصفحة الثانية عرفنا المتغيرت وعطيناها القيم الي في الكلاس والاراي اضفناها ب الابيند
 }
 
--- EhabHakami_TableViewHomeWork/EhabHakami_TableViewHomeWork/ViewControllerTwo.swift
+++ EhabHakami_TableViewHomeWork/EhabHakami_TableViewHomeWork/ViewControllerTwo.swift
@@ -9,35 +9,53 @@ import UIKit
 
 class ViewControllerTwo: UIViewController {
     
-    var recivedData = ""
+    
     @IBOutlet weak var labelViewControlerTwo: UILabel!
-    
+    var carName = ""
     @IBOutlet weak var imageViewContrrolerTwo: UIImageView!
-    
+    var imageCar = UIImage(named: "")
     @IBOutlet weak var textViewContrrolerTwo: UITextView!
+    var discrabionCar = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelViewControlerTwo.text = recivedData
-        imageViewContrrolerTwo.image = UIImage(named: recivedData)
+        //تعريف الاوبجكت مع المتغير عشان نسوي عملية التعريف حق الصفحة الثانية وتنتقل البيانات
+        labelViewControlerTwo.text = carName
         
-        if labelViewControlerTwo.text == "charger_Srt" {
-            textViewContrrolerTwo.text = "musleCar"
-            
-        }else if labelViewControlerTwo.text == "Challenger_Srt"{
-            textViewContrrolerTwo.text = "musleCar"
-            
-            
-        }else if labelViewControlerTwo.text == "comarozl1"{
-            textViewContrrolerTwo.text = "musleCar"
-            
-        }else if labelViewControlerTwo.text == "c8"{
-        textViewContrrolerTwo.text = "superCar"
-            
-        }else{
-            textViewContrrolerTwo.text = "superCar"
-        }
-        //end viewDidLoad
-    }
+        imageViewContrrolerTwo.image = imageCar
+        
+        textViewContrrolerTwo.text = discrabionCar
+        
+        
+        
+        
+        // خبصة الي في الكومنت الباقي 
+        
+//        labelViewControlerTwo.text = recivedData
+//        imageViewContrrolerTwo.image = UIImage(named: recivedData)
+//
+//        if labelViewControlerTwo.text == "charger_Srt" {
+//            textViewContrrolerTwo.text = "musleCar"
+//
+//        }else if labelViewControlerTwo.text == "Challenger_Srt"{
+//            textViewContrrolerTwo.text = "musleCar"
+//
+//
+//        }else if labelViewControlerTwo.text == "comarozl1"{
+//            textViewContrrolerTwo.text = "musleCar"
+//
+//        }else if labelViewControlerTwo.text == "c8"{
+//        textViewContrrolerTwo.text = "superCar"
+//
+//        }else{
+//            textViewContrrolerTwo.text = "superCar"
+//        }
+       
+        
+        
+        
+        
+        
+    }// end viewDidLoad
     
 }
