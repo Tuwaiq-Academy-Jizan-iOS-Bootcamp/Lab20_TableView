5d01a2a096252a2011111060d972d23427684ed9
yasir <donyasser4@gmail.com>
2021-11-15 14:32:09 +0300

    update for Section

---
+++ TigerScondProggraming/TigerScondProggraming/Model/StructOfTiger.swift
@@ -0,0 +1,13 @@
+//
+//  StructOfTiger.swift
+//  TigerScondProggraming
+//
+//  Created by Yasir Hakami on 15/11/2021.
+//
+
+import Foundation
+struct Tiger {
+    var nameOfTiger: String
+    var infoOfTiger: String
+    var imageOfTiger: String
+}
